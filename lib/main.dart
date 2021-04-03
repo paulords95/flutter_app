2457b4c21b77652098a30b9e6e1fb728937e52de
paulords95 <paulords95@gmail.com>
2021-04-03 19:22:19 -0300

    clear build

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RadnomWords(),
-        ),
-      ),
+      home: Text('Hello world'),
     );
-  }
-}
-
-class RadnomWords extends StatefulWidget {
-  @override
-  _RadnomWordsState createState() => _RadnomWordsState();
-}
-
-class _RadnomWordsState extends State<RadnomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
   }
 }
