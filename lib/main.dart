97b32e3415cd4355839be2241ff3680f1fb27b80
paulords95 <paulords95@gmail.com>
2021-04-03 19:48:31 -0300

    04041948

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion () {
+    print('Answer chosen');
+  }
+
   @override
   build(BuildContext context) {
+    var questions = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
     return MaterialApp(
-      home: Text('Hello world'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(
+          children: [
+            Text('Perguntas'),
+            RaisedButton(
+              child: Text('Answer 1'), 
+              onPressed: answerQuestion,
+              ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+              ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
